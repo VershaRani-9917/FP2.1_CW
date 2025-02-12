f1d4eeb9f7aba5888d9dc521ca1afc2884f6973c
Varsha yadav <148608112+VershaRani-9917@users.noreply.github.com>
2025-02-12 09:47:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//  .filter() method
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const evenNumbers = numbers.filter(num => num % 2 === 0)
+console.log(evenNumbers)
+
+
+
+const words = ["Apple", "Mango", "Banana", "Orange"]
+const longWords = words.filter(word => word.length > 5)
+console.log(longWords)
+
+
+// get an array with only the positive numbers
+
+const numbersArray = [2, -7, 5, 7, -8, -6, 9]
+const positiveNumberArray = numbersArray.filter(num => num > 0)
+console.log(positiveNumberArray)
+
+
+
+const fruits = ["apple", "kiwi", "orange", "grapes", "melon"]
+const fruitsContainingA = fruits.filter(fruit => fruit.includes("a"))
+console.log(fruitsContainingA)
